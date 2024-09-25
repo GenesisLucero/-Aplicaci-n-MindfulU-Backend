41a6f7b7446789458fc0342c80115d66b4b7fe19
GenesisLucero <148606419+GenesisLucero@users.noreply.github.com>
2024-09-24 22:22:38 -0500

    Update CitaRepository.java
    poner mis dos queris

--- src/main/java/edu/com/upc/mindfulu/mindfulu/repositories/CitaRepository.java
+++ src/main/java/edu/com/upc/mindfulu/mindfulu/repositories/CitaRepository.java
@@ -1,7 +1,20 @@
 package edu.com.upc.mindfulu.mindfulu.repositories;
-
 import edu.com.upc.mindfulu.mindfulu.entities.Cita;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface CitaRepository extends JpaRepository<Cita, Integer> {
+
+    // Buscar una cita por tipo de cita
+    @Query("select c from Cita c where c.tipoCita = :tipoCita")
+    List<Cita> buscarPorTipoCita(@Param("tipoCita") String tipoCita);
+
+    // Contar citas por estado
+    @Query("select count(c) from Cita c where c.estadoCita = :estadoCita")
+    int contarPorEstado(@Param("estadoCita") String estadoCita);
+    
 }
