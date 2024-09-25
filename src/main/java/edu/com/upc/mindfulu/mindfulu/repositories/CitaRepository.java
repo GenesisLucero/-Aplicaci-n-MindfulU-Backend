53785a30509faba09652bf8110d5eb8440b53aa2
Aldair Pariño <u202020356@upc.edu.pe>
2024-09-25 16:05:21 -0500

    delete

--- src/main/java/edu/com/upc/mindfulu/mindfulu/repositories/CitaRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.com.upc.mindfulu.mindfulu.repositories;
-
-import edu.com.upc.mindfulu.mindfulu.entities.Cita;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CitaRepository extends JpaRepository<Cita, Integer> {
-}
